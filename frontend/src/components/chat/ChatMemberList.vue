54e5fdbbe9aee880f052d6d9c50cfe6024980b54
jh Lim <jhwlim25@gmail.com>
2021-07-11 22:17:56 +0900

    ChatView 컴포넌트 분리

---
+++ frontend/src/components/chat/ChatMemberList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        chatlist
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,15 +1,36 @@
 <template>
-    <div>
-        chat
+    <div class="container">
+        <chat-member-list class="chat-member-list"></chat-member-list>
+        <chat-box class="chat-box"></chat-box>
     </div>
 </template>
 
 <script>
-export default {
+import ChatMemberList from '@/components/chat/ChatMemberList.vue';
+import ChatBox from '@/components/chat/ChatBox.vue';
 
+export default {
+    components: {
+        ChatMemberList,
+        ChatBox,
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+.container {
+    box-sizing: border-box;
+    margin: 60px auto;
+    max-width: 950px;
+    min-width: 768px;
+    border: 1px solid lightgray;
+    display: flex;
+    flex-direction: row;
+}
+.chat-member-list {
+    width: 240px;
+}
+.chat-box {
+    width: calc(100% - 240px);
+}
 </style>
